92ca5ffadfc141833b9b0ef1bd2648e1889380ca
11carmel11 <11carmrl11@gmail.com>
2021-11-02 13:20:54 +0200

    adjusting workspace

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const app = require("./app");
+const express = require("express");
+const cors = require("cors");
+const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(cors());
+const path = require("path");
+
+app.use("/public", express.static(`./public`));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
+module.exports = app;
