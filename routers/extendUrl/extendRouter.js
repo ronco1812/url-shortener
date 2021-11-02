4c90f34aeb60b75c7d3845c1dc0a78e7762ab5bd
11carmel11 <11carmrl11@gmail.com>
2021-11-02 20:22:12 +0200

    adding counter functionality

--- routers/extendUrl/extendRouter.js
+++ routers/extendUrl/extendRouter.js
@@ -7,6 +7,9 @@ router.get("/:id", (req, res) => {
   const data = fs.readFileSync(path.resolve(__dirname, "../database.json"));
   const savedContent = JSON.parse(data);
   const relevantContent = savedContent[id];
+  relevantContent.counter++;
+  fs.rmSync(path.resolve(__dirname, "../database.json"));
+  fs.appendFileSync("./routers/database.json", JSON.stringify(savedContent));
   res.redirect(relevantContent.URL);
 });
 module.exports = router;
